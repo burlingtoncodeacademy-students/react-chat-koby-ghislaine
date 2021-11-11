aabf2c42dde0b54bce05f5fe59da0de7f343a6a6
Ghislaine Lykens <glykens@Ghislaines-MacBook-Pro.local>
2021-11-11 16:09:59 -0500

    worked on server thurs

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//GMBL: note: I added notes, even to the boiler plate stuff because it helps me to talk it out to myself what its used for to retain it better
 // allows usage of .env to contain our "hidden variables"
 require("dotenv").config();
 // importing express server
@@ -45,11 +46,28 @@ database.on('error', console.error.bind(console, 'connection error:'))
 
 // obtains the added 'author name' from the 'submit message form on the home-page ... this should set author name to the body of request
 app.post('/submit', (req, res) => {
-  const { author } = req.body;
+  const {author} = req.body;
 });
 // maybe should add 'room' so like {author, room} so that their message will appear in THAT room
+app.post('/rooms/:roomID/message', async (req, res) => {
+  // getting properties from the request body
+  let author = req.body.author;
+  let msgBody = req.body.msgBody;
+  // make a new 'Date' obj & assign to 'date' variable (unless this date is a constructor- check)
+  let when = new Date();
+  //pull the roomID for the specific 'room' that the user is posting a message into & assign it to roomID
+  let roomID = req.params.roomID;
 
-// GMBL re-direct from homepage to the actual chat room.. or should we do all the components like 1. set author, 2. set room, 3. set message and THEN re-direct to the chat route all at same time.. like do all the app.posts and then all the re-direct or alternate 1/2, 2/2, 3/3.. idk right now im so tired. 
+  //at the same time, we can make sure that the body of the message does not exceed 500 characters, if it does- send cat error message
+  if(msgBody > 500) {
+    return res.sendStatus(403);
+  }
+  
+});
+
+
+
+// GMBL: re-direct from homepage to the actual chat room.. or should we do all the components like 1. set author, 2. set room, 3. set message and THEN re-direct to the chat route all at same time.. like do all the app.posts and then all the re-direct or alternate 1/2, 2/2, 3/3.. 
 
 
 
