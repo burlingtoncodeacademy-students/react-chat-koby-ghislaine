5b462f3fe5c162493dbd8a8d776bd24a8208ea8a
Ghislaine Lykens <glykens@Ghislaines-MacBook-Pro.local>
2021-11-12 14:02:52 -0500

    app.js and components worked on

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import css file for app.js
+import "./App.css";
+// react-router-dom imports
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// Room Component Imports
+import Home from "./components/Home.js";
+import BlightTownRoom from "./components/BlightTownRoom.js"
+import ChristmasRoom from "./components/ChristmasRoom.js"
+import RainyDayRoom from "./components/RainyDayRoom.js"
+
+// Main App
 function App() {
+  // returns our (room) routes inside of the BrowserRouter
   return (
-    <div className="App">
-      <h1>Hello, Chat!</h1>
+    <div>
+      <BrowserRouter>
+      /* purpose of BrowserRouter is for routing on the client-side using urls *\
+      <Routes>
+
+        \* contains all the individual routes *\
+        <Route
+        path = "/"
+        element = {<Home/>}
+        />
+
+        <Route
+        path = "/blight-town-room"
+        element = {<BlightTownRoom />}
+        />
+
+        <Route
+        path = "/christmas-room"
+        element = {<ChristmasRoom/>}
+        />
+
+        <Route
+        path = "/rainy-day-room"
+        element = {<RainyDayRoom/>}
+        />
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
